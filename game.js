ee13a8b4637890c4790cc69bf410cdce67dfa306
Ali Shouman <alishouman010@gmail.com>
2022-09-03 20:35:24 +0300

    js clean code fix bugs

--- game.js
+++ game.js
@@ -7,48 +7,56 @@ function run() {
   const game = document.getElementById("game");
   const boundary = document.getElementsByClassName("boundary");
   var score = 0;
-  start.addEventListener("mouseenter", startGame);
+  var lost = false;
+  var timeOut;
+  start.addEventListener("mouseover", startGame);
   start.addEventListener("click", reset);
+
   function reset() {
+    status.innerText =
+      'Resetting Game... \n Begin by moving your mouse over the "S"';
+    lost = false;
     score = 0;
+    removeEvent();
+    if (timeOut != undefined) {
+      clearTimeout(timeOut);
+    }
+    timeOut = setTimeout(startGame, 1500);
   }
 
   function startGame() {
+    lost = false;
     for (let index = 0; index < boundary.length; index++) {
       boundary[index].addEventListener("mouseenter", lose);
       boundary[index].style.backgroundColor = "#eeeeee";
     }
-    game.addEventListener("mouseleave", cheat);
+    game.addEventListener("mouseleave", lose);
     end.addEventListener("mouseenter", win);
-    status.innerText = "Good Luck your score: " + score;
-  }
-
-  function cheat() {
-    for (let index = 0; index < boundary.length; index++) {
-      boundary[index].style.backgroundColor = "red";
-      boundary[index].removeEventListener("mouseenter", lose);
-    }
-    end.removeEventListener("mouseenter", win);
-    status.innerText = "You lost your score: " + score;
+    status.innerText = "Game Starded Good Luck your score: " + score;
   }
 
   function win() {
     score += 5;
-    for (let index = 0; index < boundary.length; index++) {
-      boundary[index].removeEventListener("mouseenter", lose);
-    }
+    lost = false;
+    removeEvent();
     status.innerText = "You Win your score: " + score;
-    game.removeEventListener("mouseleave", cheat);
-    end.removeEventListener("mouseenter", win);
   }
 
   function lose() {
+    lost = true;
     score -= 10;
+    removeEvent();
+    status.innerText = "You lost your score: " + score;
+  }
+
+  function removeEvent() {
     for (let index = 0; index < boundary.length; index++) {
-      boundary[index].style.backgroundColor = "red";
+      if (lost) {
+        boundary[index].style.backgroundColor = "red";
+      }
       boundary[index].removeEventListener("mouseenter", lose);
     }
+    game.removeEventListener("mouseleave", lose);
     end.removeEventListener("mouseenter", win);
-    status.innerText = "You lost your score: " + score;
   }
 }
